fccf8c6d23addb7633cb1a3b7d08fbb309ab542f
yeyi58095 <141631770+yeyi58095@users.noreply.github.com>
2025-07-05 17:39:39 +0800

    modified the order of execution different scritpts

---
+++ Youtube12_Component/Assets/Scripts/test1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class test1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("This is a test 1");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Youtube12_Component/Assets/Scripts/test2.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class test2 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("This is a test 2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
